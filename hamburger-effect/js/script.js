d9c0d26c59ff0493b6c360f54fd55893f14f2869
Andika Chamberlin <htmlandika@gmail.com>
2017-08-13 20:09:54 +0700

    Add files via upload

---
+++ hamburger-effect/js/script.js
@@ -0,0 +1,150 @@
+window.addEventListener("load", function(){
+	var load_screen = document.getElementById("load_screen");
+	document.body.removeChild(load_screen);
+});
+
+$(document).ready(function(){
+	$(".icon").click(function(){
+		$(".icon").toggleClass("active");
+	});
+
+	$(".icon2").click(function(){
+		$(".icon2").toggleClass("active2");
+	});
+
+	$(".icon3").click(function(){
+		$(".icon3").toggleClass("active3");
+	});
+
+	$(".icon4").click(function(){
+		$(".icon4").toggleClass("active4");
+	});
+
+	$(".icon5").click(function(){
+		$(".icon5").toggleClass("active5");
+	});
+
+	$(".icon6").click(function(){
+		$(".icon6").toggleClass("active6");
+	});
+
+	$(".icon7").click(function(){
+		$(".icon7").toggleClass("active7");
+	});
+
+	$(".icon8").click(function(){
+		$(".icon8").toggleClass("active8");
+	});
+
+	$(".icon9").click(function(){
+		$(".icon9").toggleClass("active9");
+	});
+
+	$(".icon10").click(function(){
+		$(".icon10").toggleClass("active10");
+	});
+
+	$(".icon11").click(function(){
+		$(".icon11").toggleClass("active11");
+	});
+
+	$(".icon12").click(function(){
+		$(".icon12").toggleClass("active12");
+	});
+
+	$(".icon13").click(function(){
+		$(".icon13").toggleClass("active13");
+	});
+
+	$(".icon14").click(function(){
+		$(".icon14").toggleClass("active14");
+	});
+
+	$(".icon15").click(function(){
+		$(".icon15").toggleClass("active15");
+	});
+
+	$(".icon16").click(function(){
+		$(".icon16").toggleClass("active16");
+	});
+
+	$(".icon17").click(function(){
+		$(".icon17").toggleClass("active17");
+	});
+
+	$(".icon18").click(function(){
+		$(".icon18").toggleClass("active18");
+	});
+
+	$(".icon19").click(function(){
+		$(".icon19").toggleClass("active19");
+	});
+
+	$(".icon20").click(function(){
+		$(".icon20").toggleClass("active20");
+	});
+
+	$(".icon21").click(function(){
+		$(".icon21").toggleClass("active21");
+	});
+
+	$(".icon22").click(function(){
+		$(".icon22").toggleClass("active22");
+	});
+
+	$(".icon23").click(function(){
+		$(".icon23").toggleClass("active23");
+	});
+
+	$(".icon24").click(function(){
+		$(".icon24").toggleClass("active24");
+	});
+
+	$(".icon25").click(function(){
+		$(".icon25").toggleClass("active25");
+	});
+
+	$(".icon26").click(function(){
+		$(".icon26").toggleClass("active26");
+	});
+
+	$(".icon27").click(function(){
+		$(".icon27").toggleClass("active27");
+	});
+
+	$(".icon28").click(function(){
+		$(".icon28").toggleClass("active28");
+	});
+
+	$(".icon29").click(function(){
+		$(".icon29").toggleClass("active29");
+	});
+
+	$(".icon30").click(function(){
+		$(".icon30").toggleClass("active30");
+	});
+
+	$(".icon31").click(function(){
+		$(".icon31").toggleClass("active31");
+	});
+
+	$(".icon32").click(function(){
+		$(".icon32").toggleClass("active32");
+	});
+
+	$(".icon33").click(function(){
+		$(".icon33").toggleClass("active33");
+	});
+
+	$(".icon34").click(function(){
+		$(".icon34").toggleClass("active34");
+	});
+
+	$(".icon35").click(function(){
+		$(".icon35").toggleClass("active35");
+	});
+
+	$(".icon36").click(function(){
+		$(".icon36").toggleClass("active36");
+	});
+});

1cfe7b5dab03db520456a704c6ca18a419796c8b
Andika Chamberlin <htmlandika@gmail.com>
2017-11-16 08:27:36 -0800

    Update opn.js

--- one-page-nav/vendor/opn/js/opn.js
+++ one-page-nav/vendor/opn/js/opn.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	$("#nav").onePageNav();
 
-	$("#fmcToggleBasic").click(function(){
+	$("#fmcToggleBasic i").click(function(){
 		$("#fmcNavBasicItems").toggleClass("fmc-nav-basic-items-active");
 		$("#fmNavBasicWindow").toggle();
 		$(this).toggleClass("fmc-nav-basic-toggle-active");
@@ -10,7 +10,7 @@ $(document).ready(function() {
 });
 
 $(document).on("click", function(event){
-	var fmcNavBasicToggle = $("#fmcToggleBasic i");
+	var fmcNavBasicToggle = $("#fmcToggleBasic");
 
     if(fmcNavBasicToggle !== event.target && !fmcNavBasicToggle.has(event.target).length){
         $("#fmcNavBasicItems").removeClass("fmc-nav-basic-items-active");

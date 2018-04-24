dffa5f5cbb216ba14f0c5a285648a815d47b815e
Andika Chamberlin <htmlandika@gmail.com>
2018-04-25 00:28:14 +0700

    Add files via upload

---
+++ sidebar/js/sidebar.perspective.js
@@ -0,0 +1,37 @@
+/*------------------------------------------------------------------
+
+[Master Script]
+
+Code Name     : Sidebar Multi Level (perspective)
+Version       : 1.0
+Framework     : iCore For Developer // getcore.github.io
+Author        : Andika Chamberlin // andikachamberlin.github.io
+Credit        : taniarascia.com
+
+-------------------------------------------------------------------*/
+
+"use strict";
+$(function(){
+
+	// sidebar multi level
+	$(".i-acc-list li a:not(:only-child)").click(function(e) {
+      	$(this).siblings(".i-acc-dropdown").slideToggle(300);
+      	$(this).find(".i-acc-arrow").toggleClass("i-acc-arrow-active");
+      	$(this).toggleClass("i-acc-active");
+      	e.stopPropagation();
+    });
+
+    // sidebar btn show sidebar
+    $(".i-sidebar-btn").click(function(){
+      $(".i-perspective").toggleClass("i-perspective-active");
+    	$(".i-sidebar").toggleClass("i-sidebar-active");
+    	$(".i-sidebar-window").fadeIn(300);
+    });
+
+    $(".i-sidebar-window").click(function(){
+    	$(this).fadeOut(300);
+    	$(".i-sidebar").removeClass("i-sidebar-active");
+      $(".i-perspective").removeClass("i-perspective-active");
+    });
+
+})
